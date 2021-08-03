ad6f59410f48b1def11c30a3494bb94e2d62931e
D3rHummel <lukas.maibaum@tutanota.com>
2021-08-03 20:20:41 +0200

    init rest-api #3

---
+++ rest-api/src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+
+// the following as example
+// https://github.com/hagopj13/node-express-boilerplate
+// also look at this for more information:
+// https://www.coreycleary.me/project-structure-for-an-express-rest-api-when-there-is-no-standard-way/
