bcb656ae272f7de49b1449df7788c60410244203
D3rHummel <lukas.maibaum@tutanota.com>
2021-08-22 11:52:24 +0200

    update app.js #4

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const routes = require("./routes/v1");
+const ApiError = require("./utils/ApiError");
+const httpStatus = require("http-status");
 
 const app = express();
 
@@ -11,5 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // v1 api routes
 app.use("/v1", routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
 
 module.exports = app;
