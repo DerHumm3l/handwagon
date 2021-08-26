6d4d2e84f7181da2d6afa626467aabe141529569
D3rHummel <lukas.maibaum@tutanota.com>
2021-08-26 20:08:48 +0200

    update cart.model.js #4

--- rest-api/src/models/cart.model.js
+++ rest-api/src/models/cart.model.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const cartSchema = mongoose.Schema(
   {
-    accessHash: {
+    accessKey: {
       type: String,
       required: true,
       index: true,
@@ -14,6 +15,24 @@ const cartSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+/**
+ * Check if id matches the access key
+ * @param {string} id
+ * @returns {Promise<boolean>}
+ */
+cartSchema.methods.isAccessKeyMatch = async function (id) {
+  const cart = this;
+  return bcrypt.compare(id, cart.accessKey);
+};
+
+cartSchema.pre("save", async function (next) {
+  const cart = this;
+  if (cart.isModified()) {
+    cart.accessKey = await bcrypt.hash(cart.accessKey, 8);
+  }
+  next();
+});
 
 /**
  * @typedef Cart
