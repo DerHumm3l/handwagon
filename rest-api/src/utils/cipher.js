dbb90eaec4250458bdeb24d43a3d03176e6df0dd
D3rHummel <lukas.maibaum@tutanota.com>
2021-08-27 18:17:26 +0200

    add cipher jsDoc #4

--- rest-api/src/utils/cipher.js
+++ rest-api/src/utils/cipher.js
@@ -1,5 +1,19 @@
 const aesjs = require("aes-js");
 
+/**
+ * Encrypts data using AES (128-Bit) in CTR mode.
+ * @param {string} secret The 16 Bytes key for encrypting the data.
+ * @param {string} plain The data to be encrypted.
+ * @returns The encrypted data.
+ * @example
+ * const { encrypt } = require("./cipher");
+ *
+ * // The plain data to be encrypted
+ * const text = "bob ross";
+ *
+ * // encrypt the data with a secret, that is 16-Bytes long
+ * const hex = encrypt("sixteenletters!", text);
+ */
 const aesEncrypt = (secret, plain) => {
   const keyBytes = aesjs.utils.utf8.toBytes(secret);
   const textBytes = aesjs.utils.utf8.toBytes(plain);
@@ -13,6 +27,20 @@ const aesEncrypt = (secret, plain) => {
   return encryptedHex;
 };
 
+/**
+ * Decrypts data using AES (128-Bit) in CTR mode.
+ * @param {string} secret The 16 Bytes key for decrypting the data.
+ * @param {string} hex The encrypted hexadecimal data to be decrypted.
+ * @returns The decrypted data.
+ * @example
+ * const { encrypt, decrypt } = require("./cipher");
+ *
+ * // first have encrypted data
+ * const encryptedData = encrypt("sixteenletters!", "Leonardo da Vinci");
+ *
+ * // decrypt the encrypted data
+ * const decryptedData = decrypt("sixteenletters!", encryptedData);
+ */
 const aesDecrypt = (secret, hex) => {
   const keyBytes = aesjs.utils.utf8.toBytes(secret);
   const encryptedBytes = aesjs.utils.hex.toBytes(hex);
