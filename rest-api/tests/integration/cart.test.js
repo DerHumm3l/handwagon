e006eb609e7326886a3b863687ad78877df09677
D3rHummel <lukas.maibaum@tutanota.com>
2021-08-28 14:06:49 +0200

    setup cart test #4

--- rest-api/tests/integration/cart.test.js
+++ rest-api/tests/integration/cart.test.js
@@ -1,3 +1,9 @@
 const request = require("supertest");
+const faker = require("faker");
+const httpStatus = require("http-status");
 const app = require("../../src/app");
+const setupTestDB = require("../../src/utils/setupTestDB");
 const { Cart } = require("../../src/models");
+const { cartOne, insertCarts } = required("../fixtures/cart.fixture");
+
+setupTestDB();
